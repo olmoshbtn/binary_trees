decd9617c477cb9635dce5cf8b1ef795d225c940
Carlos Araque <cararqueu@gmail.com>
2021-03-10 23:25:15 +0000

    fix task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,6 +13,10 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 
 	if (tree)
 	{
+		if (tree->parent == NULL)
+		{
+			return (0);
+		}
 		depth = binary_tree_depth(tree->parent);
 		return (depth + 1);
 	}
