cab282d69a8d99b72119986c909040c649845847
Carlos Araque <cararqueu@gmail.com>
2021-03-10 23:13:48 +0000

    task 9 end

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,6 +13,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree)
 	{
+		if (tree->left == NULL && tree->right == NULL)
+		{
+			return (0);
+		}
 		tree_left = binary_tree_height(tree->left);
 		tree_right = binary_tree_height(tree->right);
 		if (tree_left > tree_right)
