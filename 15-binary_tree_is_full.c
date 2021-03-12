0a19889238e9485246b940bd8213a86684481903
olmoshbtn <2120@holbertonschool.com>
2021-03-12 00:55:13 +0000

    15-binary_tree_is_full.c

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * is_full_rec_func - checks if tree is full recursively
+ * @node: pointer to the root node of the tree to check
+ *
+ * Return: 0 if tree is not full, and 1 otherwise.
+ */
+int is_full_rec_func(const binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		if ((tree->left != NULL && tree->right == NULL) ||
+		    (tree->left == NULL && tree->right != NULL) ||
+		    is_full_rec_func(tree->left) == 0 ||
+		    is_full_rec_func(tree->right) == 0)
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 0 if not full or NULL, otherwise 1.
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (is_full_rec_func(tree));
+}
